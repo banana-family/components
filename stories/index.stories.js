cebfa948422ac0f18debe0f9e0c743b68c5249dd
Francisco Baio Dias <xicombd@gmail.com>
2018-01-04 23:32:07 +0100

    feat: add base styles component

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,9 @@
 import React from 'react'
 
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
-import { linkTo } from '@storybook/addon-links'
+import { storiesOf } from '@storybook/react-native'
 
-import { Button, Welcome } from '@storybook/react/demo'
+import { View } from 'react-native'
 
 storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
+  <View>Welcome to Storybook</View>
 ))
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ))
